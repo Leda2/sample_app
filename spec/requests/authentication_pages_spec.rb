7a27bb75e29a6495d743159ccb44147aa762de0b
Leda2 <leda2.may@gmail.com>
2014-01-01 15:54:19 +0900

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,10 @@ describe "Authentication" do
 
       it { should have_title('Sign in') }
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+			it { should_not have_link('Users', href: users_path) }
+			it { should_not have_link('Profile') }
+			it { should_not have_link('Settings') }
+			it { should_not have_link('Sign out', href: signout_path) }
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -62,6 +66,20 @@ describe "Authentication" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
           end
+
+					describe "when signing in again" do
+						before do
+							delete signout_path
+							visit signin_path
+							fill_in "Email", with:user.email
+							fill_in "Password", with:user.password
+							click_button "Sign in"
+						end
+
+						it "should render the default (profile) page" do
+							expect(page).to have_title(user.name)
+						end
+					end
         end
       end
 
@@ -82,6 +100,20 @@ describe "Authentication" do
           it { should have_title('Sign in') }
         end
       end
+
+			describe "in the Microposts controller" do
+				
+				describe "submitting to the create action" do
+					before { post microposts_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+
+				describe "submitting to the destroy action" do
+					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+
     end
 
     describe "as wrong user" do
